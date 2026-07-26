2296b108af558337c2981b0ea434c56b30c70d6e
XI4824-ArindamDas <arindam.das1@xebia.com>
2026-07-27 02:46:26 +0530

    Added Check for url reachable for frontend application

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    allowedHosts: ['app-under-test', 'localhost'],
+  },
 });
